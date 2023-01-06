7a392bd31ff8f511ec55adafa243449b40d49e70
Nikolaz Stacey <nikolazstacey@hotmail.co.uk>
2023-01-06 23:30:39 +0100

    still trying to debug solenium bug - no major changes commited

--- spec/home_page_spec.rb
+++ spec/home_page_spec.rb
@@ -1,13 +1,12 @@
 require 'rails_helper'
 
+## issues with selenium to be resolved... causing errors
 
-## issues with selenium to be resolved...
-
-# RSpec.describe 'Welcome to papatoo', type: :system do
-#   describe 'index page' do
-#     it 'shows the right content' do
-#       visit(root_path)
-#       expect(page).to have_content('Welcome to Papatoo.info')
-#     end
-#   end
-# end
+RSpec.describe 'Welcome to papatoo', type: :system do
+  describe 'index page' do
+    it 'shows the right content' do
+      visit(root_path)
+      expect(page).to have_content('Welcome to Papatoo.info')
+    end
+  end
+end
