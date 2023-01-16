8387b7c567b7dc94a48cbb2cddf83c75ee5d7878
Nikolaz Stacey <nikolazstacey@hotmail.co.uk>
2023-01-16 11:56:27 +0100

    added the article create spec

--- spec/feature_specs/new_article_spec.rb
+++ spec/feature_specs/new_article_spec.rb
@@ -4,10 +4,27 @@ RSpec.describe 'Create a new article', type: :feature do
   describe 'articles pages' do
     let(:user) { User.create(email: "article@test.com", password: "123456") }
 
+    it 'should not allow user to go to write new article page' do
+      visit(new_article_path)
+      expect(page).to have_content('Log in')
+    end
+
     it 'should not create an article without a title' do
       login_as(user)
       visit(new_article_path)
-      # finish writing test
+      fill_in 'Content', with: 'This is a rspec test article.'
+      click_on 'save'
+      expect(page).to have_content('Write a new article')
+    end
+
+    it 'should allow user to create an article and see it in the index page' do
+      login_as(user)
+      visit(new_article_path)
+      fill_in 'Title', with: 'This is an eyecatching title'
+      fill_in 'Content', with: 'This is another rspec test article.'
+      click_on 'save'
+      expect(page).to have_content('Articles')
+      expect(page).to have_content('This is an eyecatching title')
     end
   end
 end
