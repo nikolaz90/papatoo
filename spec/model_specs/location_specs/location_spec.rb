42c57996a16b537693607347d6b9f6e9f1b14991
Nikolaz Stacey <nikolazstacey@hotmail.co.uk>
2023-01-27 12:29:21 +0100

    added the spec for locations

---
+++ spec/model_specs/location_specs/location_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+RSpec.describe Comment, type: :model do
+  describe 'A can create a location' do
+    let(:valid_user) { User.create(email: 'location@test.com', password: 'testtest', username: 'location_test_1') }
+
+    it 'should not be valid without a user' do
+      location = Location.new(long: 0.00, lat: 0.00)
+      expect(location.valid?).to eq false
+    end
+
+    it 'should not be valid without long and lat' do
+      location = Location.new(user: valid_user)
+      expect(location.valid?).to eq false
+    end
+
+    it 'should not be valid without start time' do
+      location = Location.new(user: valid_user, long: 0.00, lat: 0.00)
+      expect(location.valid?).to eq false
+    end
+
+    it 'should user be able to create a valid location' do
+      location = Location.new(user: valid_user, long: 0.00, lat: 0.00, start_time: Time.now + 2.days)
+      expect(location.valid?).to eq true
+    end
+
+    it 'should be have a default duration of 30' do
+      location = Location.new(long: 0.00, lat: 0.00, user: valid_user)
+      expect(location.duration).to eq 30
+    end
+  end
+end
