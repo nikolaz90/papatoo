9ba72a05ba4bc4c6f9b96c34742155d9a797a163
Nikolaz Stacey <nikolazstacey@hotmail.co.uk>
2023-01-30 22:06:53 +0100

    added new bg to spell game and deployed

--- app/controllers/spell_this_controller.rb
+++ app/controllers/spell_this_controller.rb
@@ -1,4 +1,5 @@
 class SpellThisController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index]
   def index
     policy_scope(:spell_this)
     @top_scores = GameScore.top_3_scorers("spell this")
