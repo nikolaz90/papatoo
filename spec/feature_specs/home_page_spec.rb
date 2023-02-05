6f2f77892bf8336414347a309e95c917e9642e70
Nikolaz Stacey <nikolazstacey@hotmail.co.uk>
2023-02-05 14:05:44 +0100

    updated home page test

--- spec/feature_specs/home_page_spec.rb
+++ spec/feature_specs/home_page_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe 'Home page rspec test', type: :feature do
   describe 'home page' do
     it 'shows a welcome message with right content' do
       visit(root_path)
-      expect(page).to have_content('Welcome to Papatoo.info')
+      expect(page).to have_content('AT')
     end
   end
 end
