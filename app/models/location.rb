4ac5d9ae2324857af67a849363156734ba42e647
Nikolaz Stacey <nikolazstacey@hotmail.co.uk>
2023-02-09 22:26:24 +0100

    improving current and future location class method

--- app/models/location.rb
+++ app/models/location.rb
@@ -3,11 +3,21 @@ class Location < ApplicationRecord
 
   validates :long, :lat, :description, :start_time, presence: true
   attribute :duration, :integer, default: 30
+  attribute :start_time, :datetime, default: Time.now
+
+  scope :current_and_future_locations, -> {
+    where("end_time >= :time_now", time_now: Time.now)
+    # .or(where("end_time > :time_now", time_now: Time.now))
+  }
 
   reverse_geocoded_by :lat, :long
   after_validation :reverse_geocode
 
-  scope :current_and_future_locations, -> {
-    where("start_time >= :time_now", time_now: Time.now - 6.hours)
-  }
+  after_validation :set_end_time
+
+  private
+
+  def set_end_time
+    self.end_time = start_time + duration.minutes
+  end
 end
--- spec/model_specs/location_specs/location_spec.rb
+++ spec/model_specs/location_specs/location_spec.rb
@@ -5,18 +5,19 @@ RSpec.describe Location, type: :model do
     let(:valid_user) { User.create(email: 'location@test.com', password: 'testtest', username: 'location_test_1') }
 
     it 'should not be valid without a user' do
-      location = Location.new(long: 0.00, lat: 0.00)
+      location = Location.new(long: 0.00, lat: 0.00, start_time: Time.now)
       expect(location.valid?).to eq false
     end
 
     it 'should not be valid without long and lat' do
-      location = Location.new(user: valid_user)
+      location = Location.new(user: valid_user, start_time: Time.now)
       expect(location.valid?).to eq false
     end
 
-    it 'should not be valid without start time' do
+    it 'should have a default start time if for some reason none provided' do
       location = Location.new(user: valid_user, long: 0.00, lat: 0.00)
       expect(location.valid?).to eq false
+      expect(location.start_time.nil?).to eq false
     end
 
     it 'user should be able to create a valid location' do
@@ -35,8 +36,14 @@ RSpec.describe Location, type: :model do
     end
 
     it 'location should reverse geocode and have correct address provided from the long lat values' do
-      location = Location.create(user: valid_user,  long: 0.9254070101469551, lat: 48.913246140591184, start_time: Time.now + 2.days)
+      location = Location.create(user: valid_user, long: 0.9254070101469551, lat: 48.913246140591184, start_time: Time.now + 2.days)
       expect(location.address).to eq "Rue du Château Eau, La Butte, Nagel-Séez-Mesnil, Évreux, Eure, Normandy, Metropolitan France, 27190, France"
+    end
+
+    it 'location should reverse calculate the end time' do
+      time_now = Time.now
+      location = Location.create(user: valid_user, long: 0.9254070101469551, lat: 48.913246140591184, start_time: time_now, duration: 120)
+      expect(location.end_time).to be_within(1.second).of time_now + 120.minutes
     end
   end
 end
