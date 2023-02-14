50825387b3edd00945938d51e1802f70ef09ccb4
Nikolaz Stacey <nikolazstacey@hotmail.co.uk>
2023-02-14 23:11:34 +0100

    added custom validation on convo so one user alone cant start a convo with selves

--- app/models/convo.rb
+++ app/models/convo.rb
@@ -3,7 +3,13 @@ class Convo < ApplicationRecord
   belongs_to :receiver, foreign_key: :receiver_id, class_name: 'User'
 
   has_many :messages, dependent: :destroy
-  validates_uniqueness_of :sender_id, scope: :receiver_id
+  validates :sender_id, uniqueness: { scope: :receiver_id }
+  validate :sender_and_user_are_different
+
+  def sender_and_user_are_different
+    errors.add(:sender_id, "sender and receiver can't be the same") if sender_id == receiver_id
+  end
+
   scope :between, -> (sender_id, receiver_id) do
     where("(convos.sender_id = ? AND convos.receiver_id = ?)
       OR (convos.receiver_id = ? AND convos.sender_id = ?)",
--- spec/model_specs/convo_specs/convo_spec.rb
+++ spec/model_specs/convo_specs/convo_spec.rb
@@ -5,10 +5,15 @@ RSpec.describe Convo, type: :model do
   let(:second_valid_user) { User.create(email: 'convo2@test.com', password: 'testtest', username: 'convo_test_2') }
 
   describe 'Convo ' do
-    it 'should not be valid if the user is the same' do
+    it 'should not be valid if the sender is the same as the receiver' do
       convo = Convo.new(sender: valid_user, receiver: valid_user)
       expect(convo.valid?).to eq false
     end
-  end
 
+    it 'should not be valid if a convo between the same two user already exists' do
+      Convo.create!(sender: valid_user, receiver: second_valid_user)
+      convo_2 = Convo.new(sender: valid_user, receiver: second_valid_user)
+      expect(convo_2.valid?).to eq false
+    end
+  end
 end
