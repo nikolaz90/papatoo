d0037aed80a42130ada4ca7a73491d432c51f2d5
Nikolaz Stacey <nikolazstacey@hotmail.co.uk>
2023-02-22 19:37:39 +0100

    added flatpickr to locations create form

---
+++ app/javascript/controllers/flatpickr_controller.js
@@ -0,0 +1,14 @@
+import { Controller } from "@hotwired/stimulus"
+import flatpickr from "flatpickr";
+
+// Connects to data-controller="flatpickr"
+export default class extends Controller {
+  static targets = ['startTime']
+
+  connect() {
+    flatpickr(this.startTimeTarget, {
+      enableTime: true,
+      altInput: true
+    });
+  }
+}
