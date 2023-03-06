05d46382c1e5c1184e726dc736e83c627a1fb8cd
Nikolaz Stacey <nikolazstacey@hotmail.co.uk>
2023-03-06 10:53:57 +0100

    ** cors bug

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,10 +1,14 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  # allow do
+  #   origins 'https://nikolaz.tech'
+  #   resource '/api/v1/portfolio_articles', headers: :any, methods: [:get]
+  # end
+  # allow do
+  #   origins 'https://www.nikolaz.tech'
+  #   resource '/api/v1/portfolio_articles', headers: :any, methods: [:get]
+  # end
   allow do
-    origins 'https://nikolaz.tech'
-    resource '/api/v1/portfolio_articles', headers: :any, methods: [:get]
-  end
-  allow do
-    origins 'https://www.nikolaz.tech'
-    resource '/api/v1/portfolio_articles', headers: :any, methods: [:get]
+    origins '*'
+    resource '*', headers: :any, methods: [:get]
   end
 end
