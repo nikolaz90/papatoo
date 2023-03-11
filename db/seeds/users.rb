3322c71709dcce02b19d793d0a677340999c4b20
Nikolaz Stacey <nikolazstacey@hotmail.co.uk>
2023-03-11 21:17:19 +0100

    added songs model and fixed comment bug on article show view

--- db/seeds/users.rb
+++ db/seeds/users.rb
@@ -1,4 +1,4 @@
 puts 'creating user...'
 User.create!(email: "123@dev.com", password: "papatoo", username: "fun_guy")
 
-User.create!(email: "321@dev.com", password: "papatoo", username: "fun_gal")
+User.create!(email: "321@dev.com", password: "papatoo", username: "fun_gal", admin: true)
---
+++ test/factories/songs.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :song do
+    title { "MyString" }
+    artist { "MyString" }
+    description { "MyString" }
+  end
+end
